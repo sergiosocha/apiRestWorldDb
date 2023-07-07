3a5a68190c28fc223bb1a48c77d66e20b2e542af
Sergio Socha <sergio_socha@yahoo.com>
2023-07-07 15:53:02 -0500

    Upload Spring project files

---
+++ src/test/java/com/api/worldquery/WorldQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.worldquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
